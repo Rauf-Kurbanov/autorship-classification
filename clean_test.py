aff6cbe050aab588217ea40c23e2b5dbda1f2c54
rauf_kurbanov <kurbanov.re@gmail.com>
2016-08-23 15:57:07 +0300

    refactorig

--- clean_test.py
+++ clean_test.py
@@ -1,35 +1,25 @@
 import io
+import pickle
+import os
+import random
+import numpy as np
+import itertools as itt
+import matplotlib.pyplot as plt
 from time import time
+from random import shuffle
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import HashingVectorizer
-import numpy as np
-from random import shuffle
-# from sklearn.cross_validation import train_test_split
+from sklearn.feature_extraction.text import TfidfTransformer, HashingVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
-
-import matplotlib.pyplot as plt
-
 from sklearn.linear_model import RidgeClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import SGDClassifier, Perceptron, PassiveAggressiveClassifier
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestCentroid
-# from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils.extmath import density
-
 from sklearn import metrics
 from sklearn.pipeline import make_pipeline
-
-import os
-import random
-import itertools as itt
-import pickle
 
 import xgboost as xgb
 
@@ -90,14 +80,12 @@ def prepare_training_set(filename1, filename2):
 
 def vectorise_dataset(X_train, x_test, y_train, select_chi2=1000):
     n_features = 2 ** 16
-    # n_features = 1000
 
     hasher = HashingVectorizer(stop_words='english',
                                non_negative=True,
                                n_features=n_features)
 
     vectoriser = make_pipeline(hasher, TfidfTransformer())
-    # vectoriser = TfidfTransformer()
 
     X_train = vectoriser.fit_transform(X_train)
     x_test = vectoriser.transform(x_test)
@@ -112,9 +100,7 @@ def vectorise_dataset(X_train, x_test, y_train, select_chi2=1000):
     return X_train, x_test
 
 
-# Benchmark classifiers
 def benchmark(clf, X_train, y_train, X_test, y_test):
-    res = []
 
     print('_' * 80)
     print("Training: ")
@@ -124,58 +110,32 @@ def benchmark(clf, X_train, y_train, X_test, y_test):
     train_time = time() - t0
     print("train time: %0.3fs" % train_time)
 
-    t0 = time()
-    pred = clf.predict(X_train)
-    test_time = time() - t0
-    print("test time:  %0.3fs" % test_time)
+    def predict_and_score(X, y, data_name):
+        t0 = time()
+        pred = clf.predict(X)
+        test_time = time() - t0
+        print(data_name + " time:  %0.3fs" % test_time)
 
-    score = metrics.accuracy_score(y_train, pred)
-    print("accuracy on train:   %0.3f" % score)
+        score = metrics.accuracy_score(y, pred)
+        print("accuracy on" + data_name + ":   %0.3f" % score)
 
-    if hasattr(clf, 'coef_'):
-        print("dimensionality: %d" % clf.coef_.shape[1])
-        print("density: %f" % density(clf.coef_))
+        if hasattr(clf, 'coef_'):
+            print("dimensionality: %d" % clf.coef_.shape[1])
+            print("density: %f" % density(clf.coef_))
 
-    print("classification report for train:")
-    print(metrics.classification_report(y_train, pred))
+        print("classification report for " + data_name + ":")
+        print(metrics.classification_report(y, pred))
+        print("Classes proportion on " + data_name + ": {:1.2f} / {:1.2f}".format(*proportion(pred)))
+        print()
 
-    print("Classes proportion on train: {}".format(proportion(pred)))
+        return score
 
-    print()
-
-
-
-    # clf_descr = "TRAIN " + str(clf).split('(')[0]
-    train_score = score
-    # res.append((clf_descr, score, train_time, test_time))
-
-    # TODO eliminate code duplicate
-    t0 = time()
-    pred = clf.predict(X_test)
-    test_time = time() - t0
-    print("test time:  %0.3fs" % test_time)
-
-    score = metrics.accuracy_score(y_test, pred)
-    print("accuracy on test:   %0.3f" % score)
-
-    if hasattr(clf, 'coef_'):
-        print("dimensionality: %d" % clf.coef_.shape[1])
-        print("density: %f" % density(clf.coef_))
-
-    print("classification report for test:")
-    print(metrics.classification_report(y_test, pred))
-    print("Classes proportion on test: {}".format(proportion(pred)))
-
-    print()
-
+    train_score = predict_and_score(X_train, y_train, "train")
+    test_score = predict_and_score(X_test, y_test, "test")
 
     clf_descr = str(clf).split('(')[0]
-    test_score = score
-    # res.append((clf_descr, score, train_time, test_time))
 
-    # return clf_descr, score, train_time, test_time
     return clf_descr, train_score, test_score
-    # return res
 
 
 def train_all_models(X_train, y_train, X_test, y_test):
@@ -184,61 +144,48 @@ def train_all_models(X_train, y_train, X_test, y_test):
     piped = Pipeline([
         ('feature_selection', LinearSVC(penalty="l1", dual=False, tol=1e-3))
         , ('classification', LinearSVC())])
-    for clf, name in [(RandomForestClassifier(n_estimators=300, max_depth=5, max_features=15), "Random forest")]:
-    # for clf, name in (
-    #         (RidgeClassifier(tol=1e-2, solver="sag", alpha=1000), "Ridge Classifier")
-    #         , (Perceptron(n_iter=50, penalty='l2', alpha=0.001), "Perceptron")
-            # , (PassiveAggressiveClassifier(n_iter=50, C=0.0001), "Passive-Aggressive")
-            # # , (KNeighborsClassifier(n_neighbors=10), "kNN")
-            # , (SGDClassifier(alpha=.0001, n_iter=50, penalty="elasticnet"), "Elastic-Net penalty")
-            # , (NearestCentroid(), "NearestCentroid (aka Rocchio classifier)")
-            # , (MultinomialNB(alpha=.01), "MultinomialNB")
-            # , (BernoulliNB(alpha=.01), "BernoulliNB")
-            # , ((xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05)), "XGBoost")
-            # , (RandomForestClassifier(n_estimators=300, max_depth=3, max_features=15), "Random forest")
-            # , (piped, "LinearSVC with L1-based feature selection")
-    # ):
+
+    for clf, name in (
+            (RidgeClassifier(tol=1e-2, solver="sag", alpha=1000), "Ridge Classifier")
+            , (Perceptron(n_iter=50, penalty='l2', alpha=0.001), "Perceptron")
+            , (PassiveAggressiveClassifier(n_iter=50, C=0.0001), "Passive-Aggressive")
+            , (SGDClassifier(alpha=.0001, n_iter=50, penalty="elasticnet"), "Elastic-Net penalty")
+            , (NearestCentroid(), "NearestCentroid (aka Rocchio classifier)")
+            , (MultinomialNB(alpha=.01), "MultinomialNB")
+            , (BernoulliNB(alpha=.01), "BernoulliNB")
+            , ((xgb.XGBClassifier(max_depth=5, n_estimators=300, learning_rate=0.05)), "XGBoost")
+            , (RandomForestClassifier(n_estimators=300, max_depth=5, max_features=15), "Random forest")
+            , (piped, "LinearSVC with L1-based feature selection")
+    ):
 
         print('=' * 80)
         print(name)
         results.append(benchmark(clf, X_train, y_train, X_test, y_test))
-        # results.extend(benchmark(clf, X_train, y_train, X_test, y_test))
 
-    ## for penalty in ["l2", "l1"]:
-    # for penalty in ["l2"]:
-    #     for clf in (LinearSVC(penalty=penalty, dual=False, tol=1e-3)
-    #                 , SGDClassifier(alpha=.0001, n_iter=50, penalty=penalty)):
-    #         print('=' * 80)
-    #         print("%s penalty" % penalty.upper())
-    #         results.append(benchmark(clf, X_train, y_train, X_test, y_test))
-    #         # results.extend(benchmark(clf, X_train, y_train, X_test, y_test))
+    for penalty in ["l2", "l1"]:
+        for clf in (LinearSVC(penalty=penalty, dual=False, tol=1e-3)
+                    , SGDClassifier(alpha=.0001, n_iter=50, penalty=penalty)):
+            print('=' * 80)
+            print("%s penalty" % penalty.upper())
+            results.append(benchmark(clf, X_train, y_train, X_test, y_test))
 
     return results
 
 
 def plot_results(results):
-    # %matplotlib inline
     indices = np.arange(len(results))
 
-    # results = [[x[i] for x in results] for i in range(4)]
     results = [[x[i] for x in results] for i in range(3)]
 
-    # clf_names, score, training_time, test_time = results
-    # clf_names, score, training_time, _ = results
     clf_names, train_score, test_score = results
-
-    # training_time = np.array(training_time) / np.max(training_time)
-    # test_time = np.array(test_time) / np.max(test_time)
 
     plt.figure(figsize=(20, 8))
     plt.title("Score")
     plt.barh(indices, train_score, .2, label="training score", color='r')
     plt.barh(indices + .3, test_score, .2, label="test score", color='b')
-    # plt.barh(indices + .6, test_time, .2, label="test time", color='b')
     plt.yticks(())
     plt.legend(loc='best')
     plt.subplots_adjust(left=.25)
-    # plt.subplots_adjust()
     plt.subplots_adjust(top=.95)
     plt.subplots_adjust(bottom=.05)
 
@@ -246,46 +193,68 @@ def plot_results(results):
         plt.text(-.3, i, c)
 
     plt.show()
+    plt.savefig('foo.png')
+
+
+def seralize_dataset(data_dir, aut_pair, nfeatures, suffix=""):
+    if all(map(os.path.isfile, ["serialized/X_train" + suffix + ".p",
+                                "serialized/y_train" + suffix + ".p",
+                                "serialized/X_test" + suffix + ".p",
+                                "serialized/y_test" + suffix + ".p"])):
+        print("Serialized files already have been found")
+        return
+
+    train_fn1 = os.path.join(data_dir, aut_pair, "class1_training/class1_training.txt")
+    train_fn2 = os.path.join(data_dir, aut_pair, "class2_training/class2_training.txt")
+    X_train, y_train = prepare_training_set(train_fn1, train_fn2)
+
+    test_fn1 = os.path.join(data_dir, aut_pair, "class1_test/class1_test.txt")
+    test_fn2 = os.path.join(data_dir, aut_pair, "class2_test/class2_test.txt")
+
+    X_test, y_test = prepare_training_set(test_fn1, test_fn2)
+    X_train, X_test = vectorise_dataset(X_train, X_test, y_train, nfeatures)
+
+    pickle.dump(X_train, open("serialized/X_train" + suffix + ".p", "wb"))
+    pickle.dump(y_train, open("serialized/y_train" + suffix + ".p", "wb"))
+    pickle.dump(X_test, open("serialized/X_test" + suffix + ".p", "wb"))
+    pickle.dump(y_test, open("serialized/y_test" + suffix + ".p", "wb"))
+
+
+def deseralize_dataset(suffix):
+    print("Serializing dataset " + suffix)
+    t = time()
+    with open("serialized/X_train" + suffix + ".p", 'rb') as X_train_f,\
+            open("serialized/y_train" + suffix + ".p", 'rb') as y_train_f,\
+            open("serialized/X_test" + suffix + ".p", 'rb') as X_test_f,\
+            open("serialized/y_test" + suffix + ".p", 'rb') as y_test_f:
+        X_train = pickle.load(X_train_f)
+        y_train = pickle.load(y_train_f)
+        X_test = pickle.load(X_test_f)
+        y_test = pickle.load(y_test_f)
+    print("Serialization finished in {}s".format(time() - t))
+
+    print("X_train.shape = {}".format(X_train.shape))
+    return X_train, y_train, X_test, y_test
 
 
 def main():
     # random.seed(42)
+    data_dir = "/home/rauf/Programs/shpilman/data"
+    aut_pair = "perumov-vs-lukjarenko"
+    # aut_pair = "asimov-vs-silverberg"
+    nfeatures = 10000
+    suffix = str(nfeatures) + aut_pair
 
-    # data_dir = "/home/rauf/Programs/shpilman/data"
-    # aut_pair = "perumov-vs-lukjarenko"
-    # # aut_pair = "asimov-vs-silverberg"
-    #
-    # train_fn1 = os.path.join(data_dir, aut_pair, "class1_training/class1_training.txt")
-    # train_fn2 = os.path.join(data_dir, aut_pair, "class2_training/class2_training.txt")
-    # X_train, y_train = prepare_training_set(train_fn1, train_fn2)
-    #
-    # test_fn1 = os.path.join(data_dir, aut_pair, "class1_test/class1_test.txt")
-    # test_fn2 = os.path.join(data_dir, aut_pair, "class2_test/class2_test.txt")
-    #
-    # select_chi2 = 10000
-    # X_test, y_test = prepare_training_set(test_fn1, test_fn2)
-    # X_train, X_test = vectorise_dataset(X_train, X_test, y_train, select_chi2)
-    #
-    # pickle.dump(X_train, open("serialized/X_train.p", "wb"))
-    # pickle.dump(y_train, open("serialized/y_train.p", "wb"))
-    # pickle.dump(X_test, open("serialized/X_test.p", "wb"))
-    # pickle.dump(y_test, open("serialized/y_test.p", "wb"))
+    seralize_dataset(data_dir, aut_pair, nfeatures, suffix)
+    X_train, y_train, X_test, y_test = deseralize_dataset(suffix)
 
-    if all(map(os.path.isfile, ["serialized/X_train.p", "serialized/y_train.p",
-                            "serialized/X_test.p", "serialized/y_test.p"])):
-        X_train = pickle.load(open("serialized/X_train.p", "rb"))
-        y_train = pickle.load(open("serialized/y_train.p", "rb"))
-        X_test = pickle.load(open("serialized/X_test.p", "rb"))
-        y_test = pickle.load(open("serialized/y_test.p", "rb"))
-        print("X_train.shape = {}".format(X_train.shape))
-    else:
-        print("Serialized files not found")
-        return
-
-    dlen = X_train.shape[0] // 10
-    # X_train, y_train = X_train[:dlen, :], y_train[:dlen]
+    dlen = X_train.shape[0] // 100
+    X_train, y_train = X_train[:dlen, :], y_train[:dlen]
+    print("Cropped X_train.shape = {}".format(X_train.shape))
 
     results = train_all_models(X_train, y_train, X_test, y_test)
+    pickle.dump(results, open("serialized/results" + suffix + ".p", "wb"))
+
     plot_results(results)
 
 if __name__ == "__main__":
