421f3bb3450cc0d0afe6c75c36ea45b3cca10737
rauf_kurbanov <kurbanov.re@gmail.com>
2016-08-23 18:45:11 +0300

    fixed data batching

--- lstm.py
+++ lstm.py
@@ -1,7 +1,9 @@
 import os
+import math
 import numpy as np
+import itertools as itt
 
-from clean_test import prepare_training_set, vectorise_dataset, proportion, deseralize_dataset
+from clean_test import prepare_training_set, vectorise_dataset, proportion, deseralize_dataset, seralize_dataset
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
@@ -9,9 +11,9 @@ from keras.layers import Embedding
 from keras.layers import LSTM
 
 
-def simple_model(inp_layer):
+def simple_model(nfeatures):
     model = Sequential()
-    model.add(Embedding(inp_layer, 256, input_length=inp_layer, init='uniform'))
+    model.add(Embedding(nfeatures, 256, input_length=nfeatures, init='uniform'))
     model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
@@ -24,9 +26,9 @@ def simple_model(inp_layer):
     return model
 
 
-def lstm_model(inp_layer):
+def lstm_model(nfeatures):
     model = Sequential()
-    model.add(Embedding(inp_layer, 256, input_length=inp_layer, init='uniform'))
+    model.add(Embedding(nfeatures, 256, input_length=nfeatures, init='uniform'))
     model.add(LSTM(output_dim=128, activation='sigmoid', inner_activation='hard_sigmoid'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
@@ -40,58 +42,47 @@ def lstm_model(inp_layer):
 
 
 def main():
-    inp_layer = 1000
+    nfeatures = 1000
     batch_size = 32
     nb_epoch = 2
-
     data_dir = "/home/rauf/Programs/shpilman/data"
     aut_pair = "perumov-vs-lukjarenko"
-    # aut_pair = "asimov-vs-silverberg"
+    suffix = str(nfeatures) + aut_pair
 
-    train_fn1 = os.path.join(data_dir, aut_pair, "class1_training/class1_training.txt")
-    train_fn2 = os.path.join(data_dir, aut_pair, "class2_training/class2_training.txt")
-    X_train, y_train = prepare_training_set(train_fn1, train_fn2)
+    seralize_dataset(data_dir, aut_pair, nfeatures, suffix)
+    X_train, y_train, X_test, y_test = deseralize_dataset(suffix)
 
-    test_fn1 = os.path.join(data_dir, aut_pair, "class1_test/class1_test.txt")
-    test_fn2 = os.path.join(data_dir, aut_pair, "class2_test/class2_test.txt")
-
-    X_test, y_test = prepare_training_set(test_fn1, test_fn2)
-    X_train, X_test = vectorise_dataset(X_train, X_test, y_train, select_chi2=inp_layer)
-    #
-    # X_train, y_train, X_test, y_test = deseralize_dataset()
-
-    # Temporary data subsampling
-    dlen = X_train.shape[0] // 1000
+    dlen = X_train.shape[0] // 10000
+    dlen -= dlen % batch_size
     X_train, y_train = X_train[:dlen, :], y_train[:dlen]
-    print("X_train.shape = {}".format(X_train.shape))
+    print("Cropped X_train.shape = {}".format(X_train.shape))
 
-    tlen = X_test.shape[0] // 10
-    X_test, y_test= X_test[:dlen, :], y_test[:dlen]
-    print("X_test.shape = {}".format(X_train.shape))
+
+    tlen = X_test.shape[0] // 100
+    X_test, y_test= X_test[:tlen, :], y_test[:tlen]
+    print("X_test.shape = {}".format(X_test.shape))
     X_test = X_test.toarray()
     y_test = np.asarray(y_test)
 
-    # model = lstm_model(inp_layer)
-    model = simple_model(inp_layer)
-    # model.fit(X_train, y_train, batch_size=batch_size, nb_epoch=nb_epoch)
+    # model = lstm_model(nfeatures)
+    model = simple_model(nfeatures)
 
-    #TODO debug generator
     def batch_gen(batch_size, dlen):
         for i in range(0, dlen, batch_size):
             yield (X_train[i:i+batch_size, :].toarray(), np.asarray(y_train[i:i+batch_size]))
 
-    model.fit_generator(batch_gen(batch_size, dlen),
+    sample_gen = itt.cycle(batch_gen(batch_size, dlen))
+    model.fit_generator(sample_gen,
                         nb_epoch=nb_epoch,
                         validation_data=(X_test, y_test),
                         samples_per_epoch=dlen)
-    model.save("serialized/lstm.keras")
+    # model.save("serialized/simple.keras")
 
-
-    score, acc = model.evaluate(X_train, y_train, batch_size=batch_size)
-    print('Training score:', score)
-    print('Training accuracy:', acc)
-    pred = model.predict(x=X_train, batch_size=batch_size)
-    print("Classes proportion on train: {}".format(proportion(pred)))
+    # score, acc = model.evaluate(X_train, y_train, batch_size=batch_size)
+    # print('Training score:', score)
+    # print('Training accuracy:', acc)
+    # pred = model.predict(x=X_train, batch_size=batch_size)
+    # print("Classes proportion on train: {}".format(proportion(pred)))
 
     score, acc = model.evaluate(X_test, y_test, batch_size=batch_size)
     print('Test score:', score)
